cd63f0bc24f6b24e32c3bc2e15266fe2dbfd2e5e
Magda Kłopotek <magda.klopotek@gmail.com>
2018-08-08 13:54:35 +0200

    edit toggle

--- src/UserList/User.js
+++ src/UserList/User.js
@@ -1,11 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const User = (props) => (
-    <div>
-        {props.user.name}
-    </div>
-)
+class User extends React.Component {
+    state = {
+        isEdited: false,
+        userName: this.props.user.name
+    }
+
+    editToggle = () => {
+        this.setState({
+            isEdited: !this.state.isEdited,
+            userName: this.props.user.name
+        })
+    }
+
+    onUserNameChangedHandler = (event) => {
+        this.setState({
+            userName: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            this.state.isEdited ?
+                <div>
+                    <button
+                        onClick={this.editToggle}
+                    >
+                        Cancel
+                    </button>
+                    <input
+                        type='text'
+                        onChange={this.onUserNameChangedHandler}
+                        value={this.state.userName} />
+                    
+                </div>
+
+                :
+
+                <div>
+                    <button
+                        onClick={this.editToggle}
+                    >
+                        Edit!
+                    </button>
+                    {this.props.user.name}
+                </div>
+        )
+    }
+}
 
 User.propTypes = {
     user: PropTypes.object.isRequired
